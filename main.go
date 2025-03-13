d052621afdec3b0324c4b2fdfe611e964ee9dee7
Michael Stergianis <michaelstergianis@gmail.com>
2025-03-13 17:25:27 -0400

    Splits node into its own file and organize the symbols

--- main.go
+++ main.go
@@ -5,39 +5,12 @@ import (
 	"fmt"
 	"log"
 	"sort"
-	"strings"
 )
 
 func main() {
 	input := "hello world"
 	huffman(input)
 	return
-}
-
-type bitstringwriter struct {
-	b        bytes.Buffer
-	overflow *byte
-}
-
-func (b *bitstringwriter) Write(bitstring []byte) (int, error) {
-	return 0, nil
-}
-
-type Frequentable interface {
-	Freq() int
-}
-
-func printTree(tree *node) {
-	printTreeWithDepth(0, tree)
-}
-func printTreeWithDepth(depth int, tree *node) {
-	if tree == nil {
-		return
-	}
-	// print the depth, then the node
-	fmt.Printf("%s%s\n", strings.Repeat(" ", depth*2), tree.PrintTreeString())
-	printTreeWithDepth(depth+1, tree.left)
-	printTreeWithDepth(depth+1, tree.right)
 }
 
 func huffman(input string) (mapping map[rune]int, output []byte) {
@@ -58,58 +31,13 @@ func huffman(input string) (mapping map[rune]int, output []byte) {
 	return nil, nil
 }
 
-func makeTree(ordered []freqPair) *node {
-	nodes := make([]Frequentable, 0, 64)
+type bitstringwriter struct {
+	b        bytes.Buffer
+	overflow *byte
+}
 
-	for _, o := range ordered {
-		nodes = append(nodes, o)
-	}
-
-	for len(nodes) > 1 {
-		var newNode = node{}
-
-		switch left := nodes[0].(type) {
-		case freqPair:
-			{
-				newNode.left = &node{
-					freq:     left.freq,
-					freqPair: &left,
-				}
-			}
-		case *node:
-			{
-				newNode.left = left
-			}
-		default:
-			panic(fmt.Sprintf("unimplemented type %T: %v", left, left))
-		}
-
-		switch right := nodes[1].(type) {
-		case freqPair:
-			{
-				newNode.right = &node{
-					freq:     right.freq,
-					freqPair: &right,
-				}
-			}
-		case *node:
-			{
-				newNode.right = right
-			}
-		default:
-			panic(fmt.Sprintf("unimplemented type %T: %v", right, right))
-		}
-
-		newNode.freq = newNode.left.Freq() + newNode.right.Freq()
-		nodes = nodes[1:]
-		nodes[0] = &newNode
-		sort.SliceStable(nodes, func(i, j int) bool {
-			return nodes[i].Freq() < nodes[j].Freq()
-		})
-	}
-
-	head := nodes[0].(*node)
-	return head
+func (b *bitstringwriter) Write(bitstring []byte) (int, error) {
+	return 0, nil
 }
 
 type freqPair struct {
@@ -144,116 +72,6 @@ func computeFreqTable(input string) (ordered []freqPair) {
 	})
 
 	return
-}
-
-type node struct {
-	freq     int
-	freqPair *freqPair
-	left     *node
-	right    *node
-}
-
-func maxDepth(depth int, tree *node) int {
-	//            a               => 1 (spaces = nil)
-	//       /         \
-	//      b           c      => 2 (spaces = 11)
-	//    /   \       /   \
-	//   d     e     f     g   => 3 (spaces = 5)
-	//  / \   / \   / \   / \
-	// h   i j   k l   m n   o => 4 (spaces = 3, 1)
-
-	//             b          c
-	//         / /            c
-	//        / /             c
-	//       / /         /         \
-	//      d           e           f
-	//    /   \       /   \       /   \
-	//   h     i     j     k     l     m
-	//  / \   / \   / \   / \   / \   / \
-	// p   q r   s t   u v   w x   y z   a
-	if tree == nil {
-		return depth
-	}
-
-	leftDepth := 0
-	if tree.left != nil {
-		leftDepth = maxDepth(leftDepth, tree.left)
-	}
-
-	rightDepth := 0
-	if tree.right != nil {
-		rightDepth = maxDepth(rightDepth, tree.right)
-	}
-
-	if leftDepth > rightDepth {
-		return leftDepth + 1
-	}
-	return rightDepth + 1
-}
-
-func (n *node) Search(r rune) (string, bool) {
-	if n.freqPair != nil {
-		if n.freqPair.r == r {
-			return "", true
-		}
-		return "", false
-	}
-
-	if n.left == nil || n.right == nil {
-		return "", false
-	}
-
-	leftS, leftB := n.left.Search(r)
-	if leftB {
-		return "0" + leftS, leftB
-	}
-
-	rightS, rightB := n.right.Search(r)
-	if rightB {
-		return "1" + rightS, rightB
-	}
-
-	return "", false
-}
-
-func (n *node) String() string {
-	s := &strings.Builder{}
-	fmt.Fprintf(s, "(%d =>", n.freq)
-	if n.freqPair != nil {
-		fmt.Fprintf(s, " f%s)", n.freqPair)
-		return s.String()
-	}
-
-	if n.left != nil {
-		fmt.Fprintf(s, " *l[%s]", n.left)
-	}
-	if n.right != nil {
-		fmt.Fprintf(s, " *r[%s]", n.right)
-	}
-	fmt.Fprint(s, ")")
-	return s.String()
-}
-
-func (n *node) PrintTreeString() string {
-	s := &strings.Builder{}
-	fmt.Fprintf(s, "(%d =>", n.freq)
-	if n.freqPair != nil {
-		fmt.Fprintf(s, " f%s)", n.freqPair)
-		return s.String()
-	}
-
-	// if n.left != nil {
-	// 	fmt.Fprintf(s, " *l[%s]", n.left)
-	// }
-	// if n.right != nil {
-	// 	fmt.Fprintf(s, " *r[%s]", n.right)
-	// }
-	// fmt.Fprint(s, ")")
-	return s.String()
-}
-
-func (n *node) Freq() int {
-	return n.freq
 }
 
 const WILD_WILD_WEST = `
